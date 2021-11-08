2ebcd862dfec46f42e407357c40de0bea30cca15
Jason <insu0929@naver.com>
2021-11-08 23:48:38 +0900

    GET API(dto getmapping)

--- src/main/java/com/example/hello/controller/GetApiController.java
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -4,10 +4,11 @@
  */
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.UserRequest;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author jasonyang
@@ -27,6 +28,42 @@ public class GetApiController {
         return "hi";
     }
 
+    // http://localhost:90090/api/get/path-variable/{name}
+    @GetMapping("/path-variable/{name}")
+    public String pathVariable(@PathVariable String name){
+        System.out.println("Path Variable: " + name);
+        return "name";
+    }
 
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
+        StringBuilder sb = new StringBuilder();
+        queryParam.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println();
+            sb.append(entry.getKey() + " = " + entry.getValue()+ "\n");
+        });
+        return sb.toString();
+    }
 
+    //이걸 query parameter마다 매핑해주는건 어렵다
+    @GetMapping("query-param02")
+    public String queryParam02(@RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam int age){
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+        return "";
+    }
+    //그래서 미리 dto로 정의함
+    @GetMapping("query-param03")
+    public String queryParam03(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
 }
